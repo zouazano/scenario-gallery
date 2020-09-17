537a0c7bf5f7ae5782018d01353cb1cca60de5e6
zouazano <sou.asano11@gmail.com>
2020-09-17 13:46:54 +0900

    add mypage bookmark

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,4 +4,8 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
+  def bookmarks
+    @player = Player.find(params[:player_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'scenarios#index'
   resources :scenarios, only: %i[index show]
-  resources :players, only: %i[show]
+  resources :players, only: %i[show] do
+    get 'bookmarks'
+  end
   resources :bookmarks, only: %i[create destroy]
   resources :reviews, only: %i[create edit destroy]
 
