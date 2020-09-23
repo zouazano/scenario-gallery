8ca798c1e0624829cea4edfeb765eeed3418fb5a
zouazano <sou.asano11@gmail.com>
2020-09-23 20:03:54 +0900

    players_path added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,6 +4,11 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
+  def index
+    @q = Player.ransack(params[:q])
+    @players = @q.result(distinct: true).order(created_at: "DESC").page(params[:page]).per(30)
+  end
+
   def bookmarks
     @player = Player.find(params[:player_id])
   end
